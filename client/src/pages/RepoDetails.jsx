e403519df622272ed71d941a82f2955b55840e1e
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2024-01-27 13:13:09 +0545

    updated the blog desc part.

--- client/src/pages/RepoDetails.jsx
+++ client/src/pages/RepoDetails.jsx
@@ -5,8 +5,34 @@ const RepoDetails = () => {
     <>
       <div className="repo_details_con">
         <div className="repo_header">
-          <h2>Repository : apexa</h2>
+          <h1>Repository : apexa</h1>
           <p>Blog published : 3</p>
+        </div>
+
+        <div className="specific_commit_with_blog">
+          <div className="commit_header">
+            <h3>
+              Commit : <span>changed the server file.</span>
+            </h3>
+            <div className="branch">main</div>
+            <div className="hastag">#1</div>
+          </div>
+          <div className="line"></div>
+          <div className="blog_info_wrapper">
+            <div className="single_blog">
+              <div className="sn_num">1.</div>
+              <div className="blog_img">
+                <img
+                  src="https://cdn.hashnode.com/res/hashnode/image/upload/v1662798291554/V5fYEOF8p.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp"
+                  alt="img_blog"
+                />
+              </div>
+              <div className="tittle_blog">
+                Debugging Feb #1: How I Overcame a CORS Error when posting to
+                Dev.to API.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
