fc79d4c048c8c47d958fba48660714c8b08a1449
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2024-01-29 00:50:13 +0545

    backend for extracting the latest commit details. ✅

--- server/controller/blogController.js
+++ server/controller/blogController.js
@@ -1,17 +1,56 @@
+import axios from "axios";
+
 export const publishBlog = async (req, res) => {
-  console.log("postblogController");
+  const { repoName, userName, token } = req.body;
+  const branch = "main";
 
-  const { commitMessage, changes, codeChanges, githubRepoUrl } = req?.body;
-  // Handle the received data as needed
-  const apiKey = req.header("Authorization");
+  try {
+    const apiUrl = `https://api.github.com/repos/${userName}/${repoName}/commits/${branch}`;
 
-  console.log("commit message:", commitMessage);
-  console.log("changes:", changes);
-  console.log("code changes:", codeChanges);
-  console.log("GitHub Repo URL:", githubRepoUrl);
-  console.log("Key:", apiKey);
+    const response = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Bearer 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`,
+        "Content-Type": "application/json",
+      },
+    });
+    const commitData = response.data;
 
-  res.status(200).json({
-    data: req.body,
-  });
+    const result = {
+      commitDetails: {
+        sha: commitData.sha,
+        message: commitData.commit.message,
+        committer: commitData.commit.committer.name,
+        date: commitData.commit.committer.date,
+      },
+      repoDetails: {
+        repoName,
+        userName: "rajeshkhadka200",
+        branch: "main",
+      },
+      filesDetails: [],
+    };
+
+    commitData.files.forEach((file) => {
+      const fileDetails = {
+        filename: file.filename,
+        additions: file.additions,
+        deletions: file.deletions,
+        changes: file.changes,
+        status: file.status,
+        patch: file.patch,
+      };
+      result.filesDetails.push(fileDetails);
+    });
+
+    res.status(200).json({
+      mesg: "Successfully fetched commit data",
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      mesg: "Internal server error",
+      error,
+    });
+  }
 };
